5b651f854c9b2de45d79d15bb782a3d5b0a018a4
prt009 <pravanjanp433@gmail.com>
2026-09-08 08:03:41 +0530

    os:copy program refactor

--- os_lab/copy.cpp
+++ os_lab/copy.cpp
@@ -18,40 +18,61 @@ int main() {
         cout << "Source file does not exist. Aborting." << endl;
         return 1;
     }
-
-    // Check if destination file already exists
-    if (filesystem::exists(dst)) {
-        cout << "Destination file already exists. Aborting." << endl;
-        return 1;
-    }
-
-    // Open source file for reading
+    
     ifstream srcF(src);
-
     if (!srcF) {
         cout << "Error opening source file. Aborting." << endl;
         return 1;
     }
 
-    // Open destination file for writing
-    ofstream destF(dst);
+    // Check if destination file already exists
+    if (filesystem::exists(dst)) {
+        cout << "Destination file already exists." << endl;
+        char opt;
+        cout << "do you want to overwrite/append/abort? [o/a/A]: " << endl;
+        cin >> opt;
+        if(opt=='o'){
+          cout<<"overwriting.\n";
+        }else if (opt=='a'){
+          ofstream destF(dst,ios::app);
+          char ch;
+          while (srcF.get(ch)) {
+              destF.put(ch);
+          }
 
-    if (!destF) {
-        cout << "Error creating destination file. Aborting." << endl;
-        return 1;
+          cout << "File appended into successfully." << endl;
+
+          srcF.close();
+          destF.close();
+          return 1;
+        }else if (opt=='A'){
+          cout<<"aborting.\n";
+          return 1;
+        }else {
+          cout<<"invalid option. aborting.\n";
+          return 1;
+        }
     }
+    ofstream destF(dst);
+          char ch;
+          while (srcF.get(ch)) {
+              destF.put(ch);
+          }
+          cout << "File copied successfully." << endl;
+          srcF.close();
+          destF.close();
+
+    // Open source file for reading
+
+    // Open destination file for writing
+    // ofstream destF(dst);
+
+    // if (!destF) {
+    //     cout << "Error creating destination file. Aborting." << endl;
+    //     return 1;
+    // }
 
     // Copy contents
-    char ch;
-
-    while (srcF.get(ch)) {
-        destF.put(ch);
-    }
-
-    cout << "File copied successfully." << endl;
-
-    srcF.close();
-    destF.close();
-
+    
     return 0;
 }
